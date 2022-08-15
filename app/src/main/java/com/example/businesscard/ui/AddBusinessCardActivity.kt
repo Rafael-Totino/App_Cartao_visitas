1b7068154a581ab6d21fe06621ca318a8bf4d77f
RAFAEL BRANCO TOTINO <rafael-totino@hotmail.com>
2022-08-15 18:31:25 -0300

    add card activity and layout

---
+++ app/src/main/java/com/example/businesscard/ui/AddBusinessCardActivity.kt
@@ -0,0 +1,12 @@
+package com.example.businesscard.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.businesscard.R
+
+class AddBusinessCardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_business_card)
+    }
+}
